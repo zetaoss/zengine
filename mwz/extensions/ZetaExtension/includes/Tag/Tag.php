e2dddce4225e69150d79c31a144ef12faf3b4cd4
jmnote <opcore@gmail.com>
2025-01-01 06:44:21 +0000

    Using ${var} in strings is deprecated

--- mwz/extensions/ZetaExtension/includes/Tag/Tag.php
+++ mwz/extensions/ZetaExtension/includes/Tag/Tag.php
@@ -46,7 +46,7 @@ class Tag
         if (empty($vid)) {
             return;
         }
-        return "<div style='width:${width}px;max-width:100%;'>
+        return "<div style='width:{$width}px;max-width:100%;'>
 <div style='position:relative;padding-bottom:56.25%;'>
 <iframe style='position:absolute;width:100%;height:100%;'
 src='http://videofarm.daum.net/controller/video/viewer/Video.html?vid=$vid&play_loc=daum_tistory'
@@ -91,7 +91,7 @@ scrolling='no'></iframe>
             $height = $args['height'];
         }
         if (!empty($vid)) {
-            return "<div style='width:${width}px;max-width:100%;'>
+            return "<div style='width:{$width}px;max-width:100%;'>
 <embed src='http://player.bgmstore.net/$vid' allowscriptaccess='always' allowfullscreen='true' width='422' height='180'></embed>
 </div></div>";
         }
@@ -112,7 +112,7 @@ scrolling='no'></iframe>
             $height = $args['height'];
         }
         if (!empty($vid)) {
-            return "<div style='width:${width}px;max-width:100%;'>
+            return "<div style='width:{$width}px;max-width:100%;'>
 <div style='position:relative;padding-bottom:56.25%;'>
 <iframe style='position:absolute;width:100%;height:100%;border:0' src='https://player.vimeo.com/video/$vid' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 </div></div>";
@@ -134,7 +134,7 @@ scrolling='no'></iframe>
             $height = $args['height'];
         }
         if (!empty($vid)) {
-            return "<div style='width:${width}px;max-width:100%;'>
+            return "<div style='width:{$width}px;max-width:100%;'>
 <div style='position:relative;padding-bottom:56.25%'>
 <iframe style='position:absolute;width:100%;height:100%;'
 src='//embed.ted.com/talks/lang/ko/$vid.html'
@@ -182,7 +182,7 @@ scrolling='no'></iframe>
         }
         if (!empty($id)) {
             return "<div style='border:1px solid #3D3D3E;border-top:0'>
-<p data-height='${height}' data-theme-id='0' data-slug-hash='$id' data-default-tab='result' class='codepen'>
+<p data-height='{$height}' data-theme-id='0' data-slug-hash='$id' data-default-tab='result' class='codepen'>
 </div>
 <script async src='//assets.codepen.io/assets/embed/ei.js'></script>";
         }
@@ -209,9 +209,9 @@ scrolling='no'></iframe>
         if (empty($vid)) {
             return "";
         }
-        return str_replace(PHP_EOL, ' ', "<div><iframe class='z-embed' style='height:${height}px'
+        return str_replace(PHP_EOL, ' ', "<div><iframe class='z-embed' style='height:{$height}px'
 sandbox='allow-forms allow-popups allow-scripts allow-same-origin allow-modals'
-src='//jsfiddle.net/${vid}/embedded/result' frameborder='0'
+src='//jsfiddle.net/{$vid}/embedded/result' frameborder='0'
 allowfullscreen='allowfullscreen' scrolling='no'></iframe></div>");
     }
 
@@ -236,7 +236,7 @@ allowfullscreen='allowfullscreen' scrolling='no'></iframe></div>");
             'preview-height' => 400,
             'fontsize' => 14,
         ]);
-        return "<div><iframe src='https://codesandbox.io/embed/${id}?fontsize=${argv['fontsize']}&hidenavigation=1&view=editor' style='width:100%; height:${argv['height']}px; border:0;' sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'></iframe><iframe src='https://codesandbox.io/embed/${id}?hidenavigation=1&view=preview' style='width:100%; height:${argv['preview-height']}px; border:0;' allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking' sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'></iframe></div>";
+        return "<div><iframe src='https://codesandbox.io/embed/{$id}?fontsize={$argv['fontsize']}&hidenavigation=1&view=editor' style='width:100%; height:{$argv['height']}px; border:0;' sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'></iframe><iframe src='https://codesandbox.io/embed/{$id}?hidenavigation=1&view=preview' style='width:100%; height:{$argv['preview-height']}px; border:0;' allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking' sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'></iframe></div>";
     }
 
     public static function renderGspreadsheets($input, $args, $parser)
@@ -254,7 +254,7 @@ allowfullscreen='allowfullscreen' scrolling='no'></iframe></div>");
 
         if (!empty($id)) {
             return "<div>
-<iframe style='border-left:6px solid silver;width:calc( 100% - 6px );height:${height}px'
+<iframe style='border-left:6px solid silver;width:calc( 100% - 6px );height:{$height}px'
 src='//docs.google.com/spreadsheets/d/$id/pubhtml?gid=0&amp;single=true&amp;widget=false&amp;headers=true'
 scrolling='no'></iframe>
 </div>";
