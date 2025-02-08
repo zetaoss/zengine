2181b7a744ea2831e4ffab62370e71f03ec91fc1
jmnote <opcore@gmail.com>
2025-02-09 05:43:06 +0900

    vite.config.ts

--- mwz/skins/ZetaSkin/vue/src/components/runbox/front/types.ts
+++ mwz/skins/ZetaSkin/vue/src/components/runbox/front/types.ts
@@ -1,16 +1,4 @@
-export interface Word {
-  type: string
-  text?: string
-  list?: Word[]
-  dict?: Record<string, Word>
-}
-
 export interface Line {
-  sev: string
-  words: Word[]
-}
-
-export interface Line0 {
   sev: string
   args: unknown[]
 }
