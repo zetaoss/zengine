afd6fd9e449249d75134ec154c63b5636b360e40
jmnote <opcore@gmail.com>
2025-02-12 02:14:31 +0900

    runbox front array

--- mwz/skins/ZetaSkin/vue/src/components/runbox/front/ConsoleArg.vue
+++ mwz/skins/ZetaSkin/vue/src/components/runbox/front/ConsoleArg.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import TheToggle from './TheToggle.vue';
 
 const props = defineProps<{
   k?: string | number | null;
@@ -57,35 +58,44 @@ onMounted(() => {
 </script>
 
 <template>
-  <span class="console-arg" :class="{ 'expand-toggle': expandable }" @click="expanded = !expanded">
-    <span v-if="expandable">
-      <span v-text="expanded ? '▼ ' : '▶ '" />
-      <span v-if="expandable">({{ len }})&nbsp;</span>
-    </span>
-    <span v-if="k !== undefined" class="key text-blue-400">{{ k }}:&nbsp;</span>
-    <span :class="typ">
-      <template v-if="typ === 'array'">
-        <template v-if="summary">
-          ({{ len }}) Array
+  <span class="inline-block align-top">
+    <span class="console-arg" :class="{ 'expand-toggle': expandable }" @click="expanded = !expanded">
+      <span v-if="k !== undefined">
+        <span>
+          <TheToggle :isOpen="expanded" :class="{ invisible: !expandable }" />
+        </span>
+        <span class="key text-blue-400">{{ k }}:&nbsp;</span>
+      </span>
+      <span v-else>
+        <span v-if="expandable">
+          <TheToggle :isOpen="expanded" />
+        </span>
+      </span>
+      <span v-if="depth < 2 || (expandable && !expanded)">({{ len }})&nbsp;</span>
+      <span :class="typ">
+        <template v-if="typ === 'array'">
+          <template v-if="depth > 1 && (summary || expanded)">
+            <span>Array({{ len }})</span>
+          </template>
+          <template v-else>
+            <span>[</span>
+            <span v-for="(v, k) in obj" :key="k">
+              <ConsoleArg :arg="v" :depth="depth + 1" :summary="true" />
+              <span v-if="k !== len - 1">, </span>
+            </span>
+            <span>]</span>
+          </template>
         </template>
-        <template v-else>
-          <span>[</span>
-          <span v-for="(v, k) in obj" :key="k">
-            <ConsoleArg :arg="v" :depth="depth + 1" :summary="true" />
-            <span v-if="k !== len - 1">, </span>
-          </span>
-          <span>]</span>
-        </template>
-      </template>
-      <template v-else-if="typ === 'object'">object</template>
-      <template v-else>{{ text }}</template>
+        <template v-else-if="typ === 'object'">object</template>
+        <template v-else>{{ text }}</template>
+      </span>
     </span>
-  </span>
-  <div v-if="expandable && expanded" class="children">
-    <div v-for="(v, k) in obj" :key="k" :style="{ paddingLeft: `${depth * 2}rem` }">
-      <ConsoleArg :k="k" :arg="v" :depth="depth + 1" />
+    <div v-if="expandable && expanded" class="children">
+      <div v-for="(v, k) in obj" :key="k" :style="{ paddingLeft: `${0.5 * depth + 0.5}rem` }">
+        <ConsoleArg :k="k" :arg="v" :depth="depth + 1" />
+      </div>
     </div>
-  </div>
+  </span>
 </template>
 
 <style scoped lang="scss">
---
+++ mwz/skins/ZetaSkin/vue/src/components/runbox/front/TheToggle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  isOpen: Boolean
+})
+</script>
+
+<template>
+  <span class="inline-block text-xs p-1 transition-transform duration-200" :class="isOpen ? 'rotate-90' : 'rotate-0'">
+    ▶
+  </span>
+</template>
