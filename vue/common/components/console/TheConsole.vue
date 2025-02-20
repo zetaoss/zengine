547ee16849c74cc2acb842f742da47e4ff9221ec
jmnote <opcore@gmail.com>
2025-02-21 02:08:22 +0900

    TheConsole

---
+++ vue/common/components/console/TheConsole.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ConsoleArg from './ConsoleArg.vue';
+import { type Log } from './types';
+
+defineProps<{
+  logs: Log[]
+}>();
+</script>
+
+<template>
+  <template v-for="(log, i) in logs" :key="i">
+    <div :class="log.level" v-for="(arg, j) in log.args" :key="j">
+      <ConsoleArg :arg="arg" :depth="0" :minify="0" :expandable="true" />
+    </div>
+  </template>
+</template>
+
+<style scoped>
+.error {
+  color: red;
+}
+
+.warn {
+  color: orange;
+}
+</style>
