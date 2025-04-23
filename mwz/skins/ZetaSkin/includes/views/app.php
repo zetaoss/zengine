3ab69e2eccb6708a396976551e3a2d281a6d68f6
jmnote <opcore@gmail.com>
2025-04-24 00:57:56 +0900

    remove SkinBlade

--- mwz/skins/ZetaSkin/includes/views/app.php
+++ mwz/skins/ZetaSkin/includes/views/app.php
@@ -6,7 +6,7 @@
                     <div class="flex h-12">
                         <a href="/" class="navbar-link">
                             <img alt="zetawiki" src="/zeta.svg" class="h-6 w-6" />
-                            <span class="hidden lg:inline ml-2 text-lg font-semibold">제타위키</span></a>
+                            <span class="hidden lg:inline ml-2 text-lg font-semibold">제타위키</span>
                         </a>
                         <a href="/" class="navbar-link !text-yellow-700 dark:!text-yellow-200">위키</a>
                         <a href="/forum" class="navbar-link">포럼</a>
@@ -14,71 +14,69 @@
                     </div>
                 </div>
             </div>
-            <navbar-user-menu :user-menu='@json($userMenu)'></navbar-user-menu>
+            <navbar-user-menu :user-menu='<?= json_encode($userMenu) ?>'></navbar-user-menu>
             <div class="flex-1">
                 <c-navbar-search></c-navbar-search>
             </div>
         </div>
     </nav>
     <div class="max-w-8xl mx-auto">
-        <div class="flex flex-row {{ $hasBinders ? 'has-binders' : '' }}">
-            @if ($hasBinders)
+        <div class="flex flex-row <?= $hasBinders ? 'has-binders' : '' ?>">
+            <?php if ($hasBinders) { ?>
                 <div class="hidden md:block flex-none w-60">
                     <the-binder></the-binder>
                 </div>
-            @endif
+            <?php } ?>
             <div class="flex-auto">
                 <div class="p-4 py-8 md:m-4 md:p-8 md:border md:rounded-md bg-white dark:bg-neutral-900">
-                    <div class="text-3xl font-semibold">{!! $html_title !!}</div>
-                    <div>{!! $html_subtitle !!}</div>
+                    <div class="text-3xl font-semibold"><?= $html_title ?></div>
+                    <div><?= $html_subtitle ?></div>
                     <div class="flow-root mt-2 text-sm">
                         <div class="float-left">
-                            @if ($is_article && $action == 'view')
-                                <page-meta historyhref="{{ $pageMenu['history']['href'] }}"></page-meta>
-                            @endif
+                            <?php if ($is_article && $action == 'view') { ?>
+                                <page-meta historyhref="<?= $pageMenu['history']['href'] ?>"></page-meta>
+                            <?php } ?>
                         </div>
                         <div class="flex float-right">
-                            @foreach ($pageBtns as $k => $v)
-                                @if ($k != $action)
-                                    @if ($k == 'edit')
-                                        <a id="ca-edit" class="page-btn"
-                                            href="{{ $v['href'] }}">{{ $v['text'] }}</a>
-                                    @else
-                                        <a class="page-btn"
-                                            href="{{ $v['href'] }}">{{ $v['text'] == '읽기' ? '보기' : $v['text'] }}</a>
-                                    @endif
-                                @endif
-                            @endforeach
+                            <?php foreach ($pageBtns as $k => $v) { ?>
+                                <?php if ($k != $action) { ?>
+                                    <?php if ($k == 'edit') { ?>
+                                        <a id="ca-edit" class="page-btn" href="<?= $v['href'] ?>"><?= $v['text'] ?></a>
+                                    <?php } else { ?>
+                                        <a class="page-btn" href="<?= $v['href'] ?>"><?= $v['text'] == '읽기' ? '보기' : $v['text'] ?></a>
+                                    <?php } ?>
+                                <?php } ?>
+                            <?php } ?>
                             <page-menu>
-                                @foreach ($pageMenu as $k => $v)
+                                <?php foreach ($pageMenu as $k => $v) { ?>
                                     <li>
-                                        <a href="{{ $v['href'] }}">{{ $v['text'] }}</a>
+                                        <a href="<?= $v['href'] ?>"><?= $v['text'] ?></a>
                                     </li>
-                                @endforeach
+                                <?php } ?>
                             </page-menu>
                         </div>
                     </div>
-                    @isset($data_portlets['data-category-normal']['html-items'])
+                    <?php if (isset($data_portlets['data-category-normal']['html-items'])) { ?>
                         <div class="page-cats">
                             <ul>
-                                {!! $data_portlets['data-category-normal']['html-items'] !!}
+                                <?= $data_portlets['data-category-normal']['html-items'] ?>
                             </ul>
                         </div>
-                    @endisset
+                    <?php } ?>
                     <article id="content" class="py-6" v-pre>
-                        {!! $html_body_content !!}
+                        <?= $html_body_content ?>
                     </article>
-                    @unless ($is_specialpage)
+                    <?php if (! $is_specialpage) { ?>
                         <the-runbox></the-runbox>
                         <page-foot></page-foot>
-                    @endunless
+                    <?php } ?>
                 </div>
             </div>
-            @unless ($is_specialpage)
+            <?php if (! $is_specialpage) { ?>
                 <div class="hidden hb-lg-md-block flex-none w-60">
-                    <toc-main :datatoc='@json($data_toc)'></toc-main>
+                    <toc-main :datatoc='<?= json_encode($data_toc) ?>'></toc-main>
                 </div>
-            @endunless
+            <?php } ?>
         </div>
     </div>
     <c-layout-foot></c-layout-foot>
