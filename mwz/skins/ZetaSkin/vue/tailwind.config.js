cedc3ab530d4478f3dfe7052249f301f1a438954
jmnote <opcore@gmail.com>
2025-04-25 19:57:15 +0900

    tailwind for mustache

--- mwz/skins/ZetaSkin/vue/tailwind.config.js
+++ mwz/skins/ZetaSkin/vue/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../includes/views/**/*.php',
+    '../templates/*.mustache',
     './src/**/*.{vue,js,ts,jsx,tsx}',
     './common/**/*.{vue,js,ts,jsx,tsx}',
   ],
