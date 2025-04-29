c2e70e85758ce8dd4a43eced1c4eebc3bdd6b9e1
jmnote <opcore@gmail.com>
2025-04-29 17:32:13 +0900

    remove dot domain cookies

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -13,6 +13,15 @@ class AuthController extends Controller
         setcookie('zetawikiUserName', '', time() - 3600, '/');
         setcookie('zetawiki_session', '', time() - 3600, '/');
 
+        // remove dot domain cookies (deprecated)
+        $dot_domain = getenv('DOT_DOMAIN');
+        if ($dot_domain) {
+            setcookie('zetawikiToken', '', time() - 3600, '/', $dot_domain);
+            setcookie('zetawikiUserID', '', time() - 3600, '/', $dot_domain);
+            setcookie('zetawikiUserName', '', time() - 3600, '/', $dot_domain);
+            setcookie('zetawiki_session', '', time() - 3600, '/', $dot_domain);
+        }
+
         return ['message' => 'logged out'];
     }
 
