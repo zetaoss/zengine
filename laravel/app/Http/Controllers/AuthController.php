b41d81466a163509864cfd896d2b4ede08f0343a
jmnote <opcore@gmail.com>
2025-04-29 20:12:51 +0900

    WG_COOKIE_PREFIX

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -8,19 +8,11 @@ class AuthController extends Controller
 {
     public function logout()
     {
-        setcookie('zetawikiToken', '', time() - 3600, '/');
-        setcookie('zetawikiUserID', '', time() - 3600, '/');
-        setcookie('zetawikiUserName', '', time() - 3600, '/');
-        setcookie('zetawiki_session', '', time() - 3600, '/');
-
-        // remove dot domain cookies (deprecated)
-        $dot_domain = getenv('DOT_DOMAIN');
-        if ($dot_domain) {
-            setcookie('zetawikiToken', '', time() - 3600, '/', $dot_domain);
-            setcookie('zetawikiUserID', '', time() - 3600, '/', $dot_domain);
-            setcookie('zetawikiUserName', '', time() - 3600, '/', $dot_domain);
-            setcookie('zetawiki_session', '', time() - 3600, '/', $dot_domain);
-        }
+        $prefix = getenv('WG_COOKIE_PREFIX');
+        setcookie($prefix.'Token', '', time() - 3600, '/');
+        setcookie($prefix.'UserID', '', time() - 3600, '/');
+        setcookie($prefix.'UserName', '', time() - 3600, '/');
+        setcookie($prefix.'_session', '', time() - 3600, '/');
 
         return ['message' => 'logged out'];
     }
