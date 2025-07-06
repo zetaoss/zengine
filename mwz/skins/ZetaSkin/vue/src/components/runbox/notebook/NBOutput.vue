c5e3d2266d622d38a6fa07e205768558524bbf75
jmnote <opcore@gmail.com>
2025-07-06 11:17:23 +0900

    notebook style (#35)
    * notebook style
    
    * php vuln

--- mwz/skins/ZetaSkin/vue/src/components/runbox/notebook/NBOutput.vue
+++ mwz/skins/ZetaSkin/vue/src/components/runbox/notebook/NBOutput.vue
@@ -1,15 +1,12 @@
-<!-- File: mwz/skins/ZetaSkin/vue/src/components/runbox/BoxNotebookOutput.vue -->
+<!-- File: mwz/skins/ZetaSkin/vue/src/components/runbox/Notebook/NBOutput.vue -->
 <script setup lang="ts">
 import { computed } from 'vue';
 
-import { type Output } from './types';
+import { type Output } from '../types';
 
 const props = defineProps<{ out: Output }>();
 
-const ansi2html = (s: string) =>
-  s.replace(/</g, '&lt;')
-    .replace(/\x1b\[(\d+;)?(\d+)m/g, (_, _1, code) => code ? `<span class="ansi${code}">` : '</span>')
-  + '</span>';
+const ansi2html = (s: string) => s.replace(/</g, '&lt;').replace(/\x1b\[(\d+;)?(\d+)m/g, (_, _1, code) => code ? `<span class="ansi${code}">` : '</span>') + '</span>';
 
 const textPlain = computed(() => props.out.data?.['text/plain']?.join('') ?? '');
 const textHtml = computed(() => props.out.data?.['text/html']?.join('') ?? '');
@@ -63,72 +60,17 @@ const textJson = computed(() => props.out.data?.['application/json']?.join('') ?
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+@use './ansi.scss';
+@use './dataframe.scss';
+</style>
+
+<style lang="scss" scoped>
 .output.error {
   @apply bg-red-100 dark:bg-red-950/80;
 }
 
-.ansi30 {
-  @apply text-black dark:text-gray-500;
-}
-
-.ansi31 {
-  @apply text-red-600 dark:text-red-400;
-}
-
-.ansi32 {
-  @apply text-green-600 dark:text-green-400;
-}
-
-.ansi33 {
-  @apply text-yellow-600 dark:text-yellow-400;
-}
-
-.ansi34 {
-  @apply text-blue-600 dark:text-blue-400;
-}
-
-.ansi35 {
-  @apply text-purple-600 dark:text-purple-400;
-}
-
-.ansi36 {
-  @apply text-cyan-600 dark:text-cyan-400;
-}
-
-.ansi37 {
-  @apply text-gray-600 dark:text-gray-400;
-}
-
-.ansi90 {
-  @apply text-gray-500 dark:text-gray-600;
-}
-
-.ansi91 {
-  @apply text-red-400 dark:text-red-300;
-}
-
-.ansi92 {
-  @apply text-green-400 dark:text-green-300;
-}
-
-.ansi93 {
-  @apply text-yellow-400 dark:text-yellow-300;
-}
-
-.ansi94 {
-  @apply text-blue-400 dark:text-blue-300;
-}
-
-.ansi95 {
-  @apply text-purple-400 dark:text-purple-300;
-}
-
-.ansi96 {
-  @apply text-cyan-400 dark:text-cyan-300;
-}
-
-.ansi97 {
-  @apply text-gray-400 dark:text-gray-200;
+img {
+  @apply max-h-[30rem] max-w-[75vw];
 }
 </style>
