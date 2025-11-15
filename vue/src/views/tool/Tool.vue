e1a6e743f2529640e7cd48c99c14ee7d143610a6
jmnote <opcore@gmail.com>
2025-11-15 16:15:36 +0900

    z-card-hover

--- vue/src/views/tool/Tool.vue
+++ vue/src/views/tool/Tool.vue
@@ -3,9 +3,9 @@
   <div class="h-full grid md:grid-cols-[120px_1fr]">
     <div class="z-card p-4">
       <p class="text-lg font-bold">도구</p>
-      <RouterLink class="rl" to="/tool/common-report">통용</RouterLink>
-      <RouterLink class="rl" to="/tool/write-request">작성요청</RouterLink>
-      <RouterLink class="rl" to="/tool/frontbox">FrontBox</RouterLink>
+      <RouterLink class="rl z-card-hover" to="/tool/common-report">통용</RouterLink>
+      <RouterLink class="rl z-card-hover" to="/tool/write-request">작성요청</RouterLink>
+      <RouterLink class="rl z-card-hover" to="/tool/frontbox">FrontBox</RouterLink>
     </div>
     <div>
       <RouterView />
@@ -18,7 +18,7 @@
   @apply block p-2;
 
   &:hover {
-    @apply no-underline z-card-hover rounded;
+    @apply no-underline rounded;
   }
 }
 </style>
