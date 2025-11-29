4cf61c0bde4c7e32d20bf5a56ff760e2e7bc875c
jmnote <opcore@gmail.com>
2025-11-30 03:43:15 +0900

    avatar

--- vue/common/components/avatar/AvatarIcon.vue
+++ vue/common/components/avatar/AvatarIcon.vue
@@ -7,22 +7,17 @@ import AvatarIdenticon from './AvatarIdenticon.vue'
 
 defineProps({
   userAvatar: { type: Object as PropType<Avatar>, required: true },
-  size: { type: Number, default: 18 },
+  size: { type: Number, default: 24 },
   showBorder: { type: Boolean, default: false },
 })
 </script>
 
 <template>
-  <span class="inline-flex align-middle items-center justify-center overflow-hidden rounded-[30%] box-border"
+  <span class="inline-flex items-center justify-center align-middle overflow-hidden rounded-full box-border"
     :class="{ border: showBorder }" :style="{ height: `${size}px`, width: `${size}px` }">
-    <template v-if="userAvatar.t == 3">
-      <img class="w-full h-full" alt="gravatar" :src="`//www.gravatar.com/avatar/${userAvatar.ghash}?s=32`">
-    </template>
-    <template v-else-if="userAvatar.t == 2">
-      <AvatarLetter :name="userAvatar.name" :size="size" />
-    </template>
-    <template v-else>
-      <AvatarIdenticon :name="userAvatar.name" :size="size" />
-    </template>
+    <img v-if="userAvatar.t === 3" class="w-full h-full" alt="gravatar"
+      :src="`//www.gravatar.com/avatar/${userAvatar.ghash}?s=${size}`" />
+    <AvatarLetter v-else-if="userAvatar.t === 2" :name="userAvatar.name" :size="size" />
+    <AvatarIdenticon v-else :name="userAvatar.name" :size="size" />
   </span>
 </template>
--- vue/common/components/avatar/AvatarIdenticon.vue
+++ vue/common/components/avatar/AvatarIdenticon.vue
@@ -1,37 +1,36 @@
 <!-- AvatarIdenticon.vue -->
 <script setup lang="ts">
 import { computed } from 'vue'
-import { Md5 } from 'ts-md5'
 
 const props = defineProps({
   name: { type: String, required: true },
   size: { type: Number, default: 18 },
 })
 
-const data = computed(() => {
-  const m = Md5.hashStr(props.name)
-  const bytes = m.match(/(.{1,2})/g) || []
+const hash = (s: string) => s.split('').reduce((h, c) => (h ^ c.charCodeAt(0)) * -5, 5) >>> 2
 
-  const bits = bytes.map(b => (b[0] > '4' ? 1 : 0))
-  const rev = [...bytes].reverse()
-  const color = '#' + [0, 1, 2].map(i => `${rev[i]?.[0] ?? '0'}0`).join('')
+const identicon = computed(() => {
+  const h = hash(props.name)
+  const hue = (h % 9) * (360 / 9)
+  const color = `hsl(${hue} 95% 45%)`
 
-  const cells: { x: number; y: number }[] = []
-  for (let y = 0; y < 5; y++) {
-    const rowBits3 = bits.slice(y * 3, y * 3 + 3)
-    const rowFull = rowBits3.concat(rowBits3.slice(0, 2).reverse())
-    rowFull.forEach((on, x) => {
-      if (on) cells.push({ x, y })
-    })
-  }
+  const cells = Array.from({ length: 25 }, (_, i) => {
+    if (!(h & (1 << (i % 15)))) return null
+    const col = (i / 5) | 0
+    const row = i % 5
+    const x = col < 3 ? col : 7 - col
+    const y = row
+    return { x, y }
+  }).filter(Boolean) as { x: number; y: number }[]
 
   return { color, cells }
 })
 </script>
 
 <template>
-  <svg :style="{ width: `${size}px`, height: `${size}px` }" viewBox="0 0 6 6" xmlns="http://www.w3.org/2000/svg"
-    shape-rendering="crispEdges">
-    <rect v-for="(c, i) in data.cells" :key="i" :x="c.x + 0.5" :y="c.y + 0.5" width="1" height="1" :fill="data.color" />
+  <svg :width="size" :height="size" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges"
+    role="img" :aria-label="`Identicon for ${props.name}`" class="bg-white">
+    <rect v-for="(c, i) in identicon.cells" :key="i" :x="c.x * 2 + 1" :y="c.y * 2 + 1" width="2" height="2"
+      :fill="identicon.color" />
   </svg>
 </template>
