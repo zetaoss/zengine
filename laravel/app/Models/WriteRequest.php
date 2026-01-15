c06b55ac3d30fee8ebe1a34c4abf1bd62112a2b3
jmnote <opcore@gmail.com>
2026-01-16 02:35:03 +0900

    avatar

--- laravel/app/Models/WriteRequest.php
+++ laravel/app/Models/WriteRequest.php
@@ -2,19 +2,16 @@
 
 namespace App\Models;
 
-use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class WriteRequest extends Model
 {
-    protected $attributes = ['rate' => 0];
-
-    protected $appends = ['hit'];
-
-    public function getHitAttribute(): int
-    {
-        $row = DB::table('not_matches')->where('title', '=', $this->title)->first();
-
-        return $row ? (int) $row->hit : 0;
-    }
+    protected $fillable = [
+        'title',
+        'user_id',
+        'user_name',
+        'rate',
+        'ref',
+        'hit',
+    ];
 }
