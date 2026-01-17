1cba88b1dc2533dd84babe24ffa840b63c37a3ec
jmnote <opcore@gmail.com>
2026-01-18 01:28:34 +0900

    Refactor Vite config (#91)
    * Refactor Vite config
    
    * Refactor Makefile and Vitest conf
    
    * Refactor Makefile and Vitest conf

--- mwz/skins/ZetaSkin/vue/vitest.config.ts
+++ mwz/skins/ZetaSkin/vue/vitest.config.ts
@@ -1,16 +1,19 @@
+// mwz/skins/ZetaSkin/vue/vitest.config.ts
 import { fileURLToPath } from 'node:url'
 
-import { configDefaults,defineConfig, mergeConfig } from 'vitest/config'
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+      },
+    }),
+  ),
 )
