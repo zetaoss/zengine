76c7b6e6c92199440f84be28ffffddee00664645
jmnote <opcore@gmail.com>
2026-01-17 22:40:16 +0900

    Refactor Vue loading

--- mwz/skins/ZetaSkin/vue/vite.config.ts
+++ mwz/skins/ZetaSkin/vue/vite.config.ts
@@ -1,3 +1,4 @@
+// mwz/skins/ZetaSkin/vue/vite.config.ts
 import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
@@ -6,16 +7,23 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig, type Plugin } from 'vite'
 
 function devDist(): Plugin {
+  let base = '/'
+
   return {
     name: 'dev-dist',
     apply: 'serve',
+    configResolved(config) {
+      base = config.base
+    },
     configureServer() {
       const distDir = path.resolve(__dirname, '../resources/dist')
-      const appJsContent = `;(function () {
-  var s = document.createElement('script')
+      const appSrc = `${base}src/app.ts?v=${Date.now()}`
+      const appJsContent = `;(() => {
+  const s = document.createElement('script')
   s.type = 'module'
-  s.src = '/dev5174/src/app.ts?v=' + Date.now()
-  document.head.appendChild(s)})()`
+  s.src = '${appSrc}'
+  document.head.appendChild(s)
+})()`
       fs.mkdirSync(distDir, { recursive: true });
       fs.writeFileSync(path.join(distDir, 'app.css'), '/* dev */', 'utf-8')
       fs.writeFileSync(path.join(distDir, 'app.js'), appJsContent, 'utf-8')
