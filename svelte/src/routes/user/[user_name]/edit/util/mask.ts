b24cafdb6ac5baabe5723a26c2512269d8243ca6
jmnote <opcore@gmail.com>
2026-02-18 02:04:07 +0900

    Update email masking logic and tests

--- svelte/src/routes/user/[user_name]/edit/util/mask.ts
+++ svelte/src/routes/user/[user_name]/edit/util/mask.ts
@@ -1,8 +1,3 @@
 export function maskEmail(email: string): string {
-  const [name = '', domainPart = ''] = email.split('@')
-  const [host = '', tld = ''] = domainPart.split('.')
-
-  const maskPart = (part: string): string => part.slice(0, 2) + '*'.repeat(Math.max(0, part.length - 2))
-
-  return `${maskPart(name)}@${maskPart(host)}.${maskPart(tld)}`
+  return email.replace(/[^@.]+/g, (part) => part.slice(0, 2) + '*'.repeat(Math.max(0, part.length - 2)))
 }
