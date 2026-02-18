93cdd9bc0190f4d6ed4763d848e600033e8775e1
jmnote <opcore@gmail.com>
2026-02-18 23:21:28 +0900

    Switch to stylelint-config-idiomatic-order

--- svelte/.stylelintrc.mjs
+++ svelte/.stylelintrc.mjs
@@ -1,14 +1,4 @@
-/** @typedef {import('./stylelint-config-recess-order-groups.d.ts')} _StylelintGroupsTypes */
-import propertyGroups from 'stylelint-config-recess-order/groups';
-
 export default {
 	customSyntax: 'postcss-html',
-	plugins: ['stylelint-order'],
-	rules: {
-		'order/properties-order': propertyGroups.map((group) => ({
-			...group,
-			emptyLineBefore: 'always',
-			noEmptyLineBetween: true,
-		})),
-	},
+	extends: ['stylelint-config-idiomatic-order'],
 };
--- svelte/stylelint-config-recess-order-groups.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'stylelint-config-recess-order/groups' {
-	const groups: Array<Record<string, unknown>>;
-	export default groups;
-}
