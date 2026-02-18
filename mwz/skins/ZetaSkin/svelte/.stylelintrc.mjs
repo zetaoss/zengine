a5bf80414dd1bcbeb2e975dbe29d335ea30c373d
jmnote <opcore@gmail.com>
2026-02-18 23:06:40 +0900

    Add Stylelint to Svelte lint workflows

---
+++ mwz/skins/ZetaSkin/svelte/.stylelintrc.mjs
@@ -0,0 +1,14 @@
+/** @typedef {import('./stylelint-config-recess-order-groups.d.ts')} _StylelintGroupsTypes */
+import propertyGroups from 'stylelint-config-recess-order/groups'
+
+export default {
+  customSyntax: 'postcss-html',
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/properties-order': propertyGroups.map((group) => ({
+      ...group,
+      emptyLineBefore: 'always',
+      noEmptyLineBetween: true,
+    })),
+  },
+}
---
+++ svelte/stylelint-config-recess-order-groups.d.ts
@@ -0,0 +1,4 @@
+declare module 'stylelint-config-recess-order/groups' {
+	const groups: Array<Record<string, unknown>>;
+	export default groups;
+}
