404a6d4769510c98b24e1075165b1d8acbbb10ab
jmnote <opcore@gmail.com>
2026-02-21 18:44:14 +0900

    Add Facebook social deletion endpoint and tests

--- laravel/app/Models/UserSocial.php
+++ laravel/app/Models/UserSocial.php
@@ -7,18 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class UserSocial extends Model
 {
     protected $table = 'zetawiki.user_social';
-
     protected $primaryKey = 'id';
-
     public $timestamps = false;
-
     protected $fillable = [
         'provider',
         'social_id',
         'user_id',
         'deletion_code',
     ];
-
     protected $casts = [
         'id' => 'int',
         'user_id' => 'int',
