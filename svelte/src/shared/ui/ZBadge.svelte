fff9c085652dfdf9108fdf4c27085779c1177df0
jmnote <opcore@gmail.com>
2026-02-27 20:08:12 +0900

    ui (#208)
    * ui

---
+++ svelte/src/shared/ui/ZBadge.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let pill = true
+
+  $: classes = [
+    'inline-flex items-center justify-center bg-[#9993] border px-1 text-xs font-medium leading-none',
+    pill ? 'rounded-full' : 'rounded',
+    typeof $$props.class === 'string' ? $$props.class : '',
+  ]
+    .filter(Boolean)
+    .join(' ')
+</script>
+
+<span class={classes}>
+  <slot />
+</span>
