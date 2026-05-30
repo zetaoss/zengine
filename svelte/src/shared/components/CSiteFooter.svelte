ca84717838c64c4ef49c9a6f056b689d27c0177f
jmnote <opcore@gmail.com>
2026-05-31 03:37:11 +0900

    css

--- svelte/src/shared/components/CSiteFooter.svelte
+++ svelte/src/shared/components/CSiteFooter.svelte
@@ -17,7 +17,7 @@
   })
 </script>
 
-<footer id="footer" class="bg-gray-400 p-6 pb-9 text-center text-[#fff]">
+<footer id="footer" class="bg-gray-400 p-6 pb-9 text-center text-(--raw-white)">
   <img alt="logo" class="mb-3 w-24" src="//storage.googleapis.com/zpub/logo.png" />
   <p class="mb-3">CC-BY-SA 3.0 · Powered by MediaWiki</p>
 
